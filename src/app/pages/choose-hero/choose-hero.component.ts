6cebb45ead4412c722adb1e0a5252e74e3b17ab3
MyroslavSB <myroslavsobchyshyn@gmail.com>
2021-09-13 18:34:32 +0300

    Output selected hero's id

--- src/app/pages/choose-hero/choose-hero.component.ts
+++ src/app/pages/choose-hero/choose-hero.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {GetHeroesService} from "../../services/get-heroes.service";
+import {FormControl, FormGroup} from "@angular/forms";
+//import {GetHeroesService} from "../../services/get-heroes.service";
 import {numRegExp} from "../../utils/constants";
-import {catchError} from "rxjs/operators";
 import {Hero} from "../../interfaces";
 
 @Component({
@@ -13,7 +12,7 @@ import {Hero} from "../../interfaces";
 export class ChooseHeroComponent implements OnInit {
 
   constructor(
-    private data: GetHeroesService
+    //private data: GetHeroesService
   ) { }
 
   letterArr = ['Q','W','E','R','T','Y','U','I','O','P', 'A','S','D','F','G','H','J','K','L', 'Z','X','C','V','B','N','M' ]
